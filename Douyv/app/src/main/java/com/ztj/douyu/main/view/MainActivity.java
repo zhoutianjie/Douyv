29a8ef259513c3ce2dc3701b1020c8b3ab87433d
Alex <kkledvcent@126.com>
2018-07-01 21:18:02 +0800

    2018-7-1

--- Douyv/app/src/main/java/com/ztj/douyu/main/view/MainActivity.java
+++ Douyv/app/src/main/java/com/ztj/douyu/main/view/MainActivity.java
@@ -1,15 +1,25 @@
 package com.ztj.douyu.main.view;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.ztj.douyu.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private RadioGroup mRadioGroup;
+    private RadioButton mRadioButtonHome;
+    private RadioButton mRadioButtonClassify;
+    private RadioButton mRadioButtonlike;
+    private RadioButton mRadioButtonMine;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initView();
     }
 }
