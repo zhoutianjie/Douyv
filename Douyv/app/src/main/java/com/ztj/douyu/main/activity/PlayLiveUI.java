9782989e08f9da1b73a364dbb4ffc3c72adba29a
Alex <kkledvcent@126.com>
2018-07-23 22:48:42 +0800

    ijkplayer 集成

--- Douyv/app/src/main/java/com/ztj/douyu/main/activity/PlayLiveUI.java
+++ Douyv/app/src/main/java/com/ztj/douyu/main/activity/PlayLiveUI.java
@@ -1,6 +1,7 @@
 package com.ztj.douyu.main.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,10 @@ import com.ztj.douyu.R;
 import com.ztj.douyu.main.presenter.PlayLivePresenter;
 import com.ztj.douyu.main.view.onPlayLiveView;
 import com.ztj.douyu.utils.StringUtils;
+import com.ztj.douyu.widgt.media.IjkVideoView;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 /**
  *直播播放页面(普通游戏的非全屏的播放页面，颜值和全屏的播放页面实现后面再研究)
@@ -15,6 +20,7 @@ import com.ztj.douyu.utils.StringUtils;
 public class PlayLiveUI extends AppCompatActivity implements onPlayLiveView {
 
     private String roomId;
+    private IjkVideoView mVideoView;
     private PlayLivePresenter presenter;
 
     @Override
@@ -45,7 +51,9 @@ public class PlayLiveUI extends AppCompatActivity implements onPlayLiveView {
     }
 
     private void initView() {
-
+        mVideoView = findViewById(R.id.ijkplayView);
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
     }
 
     private void register() {
@@ -66,13 +74,20 @@ public class PlayLiveUI extends AppCompatActivity implements onPlayLiveView {
     }
 
     @Override
-    public void onGetLiveRoomUrlInfo(String liveUrl) {
+    public void onGetLiveRoomUrlInfo(final String liveUrl) {
         if(StringUtils.isNull(liveUrl))return;
 
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 //初始化播放器
+                mVideoView.setVideoURI(Uri.parse(liveUrl));
+                mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(IMediaPlayer mp) {
+                        mVideoView.start();
+                    }
+                });
             }
         });
     }
