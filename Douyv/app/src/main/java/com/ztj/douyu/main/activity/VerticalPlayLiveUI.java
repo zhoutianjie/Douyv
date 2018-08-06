cb43e742b57fcfb05b9f72d759cacace885a7a8d
zhoutianjie <kkledvcent@126.com>
2018-08-06 17:00:47 +0800

    数据加载

--- Douyv/app/src/main/java/com/ztj/douyu/main/activity/VerticalPlayLiveUI.java
+++ Douyv/app/src/main/java/com/ztj/douyu/main/activity/VerticalPlayLiveUI.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -81,7 +82,22 @@ public class VerticalPlayLiveUI extends AppCompatActivity{
     }
 
     private void registerListeners(){
+        verticalViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
 
--- Douyv/app/src/main/java/com/ztj/douyu/main/fragment/VerticalPlayFragment.java
+++ Douyv/app/src/main/java/com/ztj/douyu/main/fragment/VerticalPlayFragment.java
@@ -1,6 +1,7 @@
 package com.ztj.douyu.main.fragment;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,19 +17,28 @@ import com.bumptech.glide.request.RequestOptions;
 import com.google.gson.Gson;
 import com.ztj.douyu.R;
 import com.ztj.douyu.bean.RoomInfo;
+import com.ztj.douyu.main.presenter.PlayLivePresenter;
+import com.ztj.douyu.main.view.onPlayLiveView;
 import com.ztj.douyu.utils.StringUtils;
+import com.ztj.douyu.widgt.media.IjkVideoView;
 
 import jp.wasabeef.glide.transformations.BlurTransformation;
+import tv.danmaku.ijk.media.player.IMediaPlayer;
 
 /**
  * Created by zhoutianjie on 2018/8/2.
  */
 
-public class VerticalPlayFragment extends Fragment{
+public class VerticalPlayFragment extends Fragment implements onPlayLiveView {
 
     private RoomInfo mRoomInfo;
     private View mRootView;
     private ImageView mRoomImage;
+    private IjkVideoView mVideoView;
+    private PlayLivePresenter presenter;
+
+    private boolean isInitView = false;
+    private boolean isLoadSuccess=false;
 
     public static VerticalPlayFragment newInstance(RoomInfo info){
         Log.e("VerticalPlayFragment","new");
@@ -41,11 +51,19 @@ public class VerticalPlayFragment extends Fragment{
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        presenter = new PlayLivePresenter();
+        presenter.attachView(this);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_vertical_play,null);
         mRoomImage = mRootView.findViewById(R.id.vertical_Image);
+        mVideoView = mRootView.findViewById(R.id.fragment_vertical_play);
         Bundle bundle = getArguments();
         if(bundle!=null){
             String jsonInfo = bundle.getString("room_info");
@@ -65,13 +83,92 @@ public class VerticalPlayFragment extends Fragment{
                         .into(mRoomImage);
             }
         }
+        isInitView = true;
+        isCanRequestData();
 
         return mRootView;
     }
 
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        isCanRequestData();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        isInitView = false;
+        isLoadSuccess = false;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        presenter.detachView();
+    }
+
+    /**
+     * 请求播放源
+     */
+    private void isCanRequestData() {
+        //尚未初始化界面，则不请求
+        if(!isInitView){
+            return;
+        }
+        if(getUserVisibleHint() &&!isLoadSuccess){
+            Log.e("FragmentV",mRoomInfo.getNickName()+" request url");
+            lazyLoad();
+        }else{
+           if(mVideoView!=null && mVideoView.isPlaying()){
+               Log.e("FragmentV",mRoomInfo.getNickName()+" release");
+               mVideoView.release(true);
+           }
+        }
+    }
+
+
+    /**
+     * 懒加载请求数据
+     */
+    private void lazyLoad() {
+        presenter.getLiveRoomUrl(mRoomInfo.getRoomId());
+    }
+
+
+    @Override
+    public void onGetLiveRoomUrlInfo(final String liveUrl) {
+        Log.e("FragmentV",mRoomInfo.getNickName()+" liveUrl"+liveUrl);
+        if(StringUtils.isNull(liveUrl))return;
+        isLoadSuccess = true;
+        Log.e("FragmentV","liveUrl is not null");
+        mRootView.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("FragmentV","setOnPreparedListener");
+                mVideoView.setVideoURI(Uri.parse(liveUrl));
+                mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(IMediaPlayer mp) {
+                        Log.e("FragmentV",mRoomInfo.getNickName()+"start");
+                        mVideoView.start();
+                        mRoomImage.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onGetLiveRoomUrlInfoFail(String message) {
 
     }
 }
